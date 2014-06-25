d0399dda9da90ce89f38bc670bf1af3165823d03
James Cryer <james.cryer@bbc.co.uk>
2014-06-25 15:56:29 +0100

    MYLOC-60 - Adding .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,32 @@
 /*global module:false*/
 module.exports = function(grunt) {
-  'use strict';
+  "use strict";
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     // Task configuration.
     jshint: {
+      options: {
+        jshintrc: true
+      },
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: "Gruntfile.js"
       }
     },
 
     watch: {
       gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: ["jshint:gruntfile"]
       }
     }
   });
 
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
 
   // Default task.
-  grunt.registerTask('build', ['jshint']);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask("build", ["jshint"]);
+  grunt.registerTask("default", ["build"]);
 
 };
