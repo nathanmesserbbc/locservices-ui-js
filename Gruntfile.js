1da9dbe42263e30b061a3b7947b97596143674e7
James Cryer <james.cryer@bbc.co.uk>
2014-06-25 15:50:22 +0100

    MYLOC-60 - Adding Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Task configuration.
+    jshint: {
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('build', ['jshint']);
+  grunt.registerTask('default', ['build']);
+
+};
