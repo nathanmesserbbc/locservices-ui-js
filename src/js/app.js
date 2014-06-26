f7a0509a83b77766fcf38498420f5f04a997bd87
James Cryer <info@jamescryer.com>
2014-06-26 21:24:19 +0100

    MYLOC-54 - Adding CSS to page

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,8 @@ require(["jquery-1"], function(jQuery) {
   require.config({ baseUrl: "/" });
 
   require([
-    "./js/component/search",
-    "./vendor/locator-core-js/src/api"
+    "js/component/search",
+    "vendor/locator-core-js/src/api"
   ], function(Search, Api) {
 
     var search = new Search({
