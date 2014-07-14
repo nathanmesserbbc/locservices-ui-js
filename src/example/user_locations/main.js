7f1b27aad4cb3d6a31c438aa96dfe0f8b4618f38
Tom Collins <tom.collins@bbc.co.uk>
2014-07-14 12:03:39 +0100

    Updating user locations example location data.

--- src/example/user_locations/main.js
+++ src/example/user_locations/main.js
@@ -29,28 +29,28 @@ require([
 
 
   var locations = [
-    {id: "1234", name: 'Llandaff', container: 'Cardiff', placeType: 'settlement', country: 'GB'},
-    {id: "5678", name: 'The Mumbles', container: 'Swansea', placeType: 'settlement', country: 'GB'},
-    {id: "1324", name: 'London', container: 'Greater London', placeType: 'settlement', country: 'GB'},
-    {id: "5681", name: 'Salford', container: 'Salford', placeType: 'settlement', country: 'GB'},
-    {id: "3453", name: 'Bristol', container: 'Bristol', placeType: 'settlement', country: 'GB'},
-    {id: "8778", name: 'Bridgend', container: 'Aberdeenshire', placeType: 'settlement', country: 'GB'},
-    {id: "8778", name: 'Belfast City Airport', container: 'Belfast', placeType: 'settlement', country: 'GB'},
+    {id: "2644160", name: 'Llandaff', container: 'Cardiff', placeType: 'settlement', country: 'GB'},
+    {id: "2641971", name: 'The Mumbles', container: 'Swansea', placeType: 'settlement', country: 'GB'},
+    {id: "2643743", name: 'London', container: 'Greater London', placeType: 'settlement', country: 'GB'},
+    {id: "2638671", name: 'Salford', container: 'Salford', placeType: 'settlement', country: 'GB'},
+    {id: "2654675", name: 'Bristol', container: 'Bristol', placeType: 'settlement', country: 'GB'},
+    {id: "2654755", name: 'Bridgend', container: 'Aberdeenshire', placeType: 'settlement', country: 'GB'},
+    {id: "6296570", name: 'Belfast City Airport', container: 'Belfast', placeType: 'settlement', country: 'GB'},
     {id: "2654710", name: 'Brighton', container: 'Brighton and Hove', placeType: 'settlement', country: 'GB'},
     {id: "2652002", name: 'Crewe', container: 'Cheshire East', placeType: 'settlement', country: 'GB'},
     {id: "2645425", name: 'Hull', container: 'Kingston upon Hull', placeType: 'settlement', country: 'GB'},
-    {id: "1", name: 'M1 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
-    {id: "4", name: 'M4 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
-    {id: "42", name: 'M42 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
-    {id: "5", name: 'M5 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
-    {id: "7891231", name: 'Paris', container: 'France', placeType: 'settlement', country: 'FR'},
-    {id: "7891232", name: 'Lille', container: 'France', placeType: 'settlement', country: 'FR'},
-    {id: "7891233", name: 'Marseille', container: 'France', placeType: 'settlement', country: 'FR'},
-    {id: "7891234", name: 'Calais', container: 'France', placeType: 'settlement', country: 'FR'},
-    {id: "7891231", name: 'New York', container: 'United States', placeType: 'settlement', country: 'US'},
-    {id: "7891232", name: 'Washington', container: 'United States', placeType: 'settlement', country: 'US'},
-    {id: "7891233", name: 'Boston', container: 'United States', placeType: 'settlement', country: 'US'},
-    {id: "7891234", name: 'Chicago', container: 'United States', placeType: 'settlement', country: 'US'}
+    {id: "8714914", name: 'M1 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
+    {id: "8715007", name: 'M4 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
+    {id: "8715009", name: 'M42 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
+    {id: "8715013", name: 'M5 (Motorway)', container: 'United Kingdom', placeType: 'road', country: 'GB'},
+    {id: "2988507", name: 'Paris', container: 'France', placeType: 'settlement', country: 'FR'},
+    {id: "2998324", name: 'Lille', container: 'France', placeType: 'settlement', country: 'FR'},
+    {id: "2995469", name: 'Marseille', container: 'France', placeType: 'settlement', country: 'FR'},
+    {id: "3029162", name: 'Calais', container: 'France', placeType: 'settlement', country: 'FR'},
+    {id: "5128581", name: 'New York', container: 'United States', placeType: 'settlement', country: 'US'},
+    {id: "4140963", name: 'Washington', container: 'United States', placeType: 'settlement', country: 'US'},
+    {id: "4930956", name: 'Boston', container: 'United States', placeType: 'settlement', country: 'US'},
+    {id: "4887398", name: 'Chicago', container: 'United States', placeType: 'settlement', country: 'US'}
   ];
   $('.addLocation').on('click', function(e){
     e.preventDefault();
