e75f4d8ecdf3091d89d33f0ff41a5ffa7a8dcadd
James Cryer <james.cryer@bbc.co.uk>
2014-07-30 17:11:34 +0100

    MYLOC-54 - Adding skeleton output

--- src/js/component/search.js
+++ src/js/component/search.js
@@ -5,25 +5,43 @@ define(['jquery', 'locservices/ui/component/component'], function($, Component) 
   'use strict';
 
   function Search(options) {
+    var self = this;
     options = options || {};
     options.componentId = 'search';
 
     if (undefined === options.api) {
       throw new Error('Search requires api parameter');
     } else {
-      this.api = options.api;
+      self.api = options.api;
     }
-    this.setComponentOptions(options);
+    self.setComponentOptions(options);
+    self.input = self.container.find(options.selector || '#ls-search-input');
+
+    self.container.on('submit', function(e){
+      e.preventDefault();
+      self.search(self.input.val());
+    });
   }
   Search.prototype = new Component();
   Search.prototype.constructor = Search;
 
-  Search.prototype.render = function() {
+  Search.prototype.search = function(searchTerm, startOffset) {
+    var params = {};
 
-  };
-
-  Search.prototype.search = function() {
-
+    $.emit(this.eventNamespace + ':start', [searchTerm]);
+    this.api.search(searchTerm, {
+      params: params,
+      success: function(data) {
+        $.emit(this.eventNamespace + ':end');
+        data.metadata.startOffset = startOffset;
+        data.metadata.searchTerm = searchTerm;
+        $.emit(this.eventNamespace + ':results', [data.results, data.metadata]);
+      },
+      error: function() {
+        $.emit(this.eventNamespace + ':end');
+        $.emit(this.eventNamespace + ':error', ['Error searching for "' +searchTerm +'"']);
+      }
+    });
   };
 
   return Search;
--- test/specs/component/searchSpec.js
+++ test/specs/component/searchSpec.js
@@ -1,19 +1,23 @@
 /*global describe, beforeEach, locservices, it:false*/
 
-define(['locservices/ui/component/search', 'locservices/ui/translations/en'], function(Search, En) {
+define([
+  'jquery',
+  'locservices/ui/component/search',
+  'locservices/ui/translations/en'
+], function($, Search, En) {
 
   describe('The search', function() {
     'use strict';
 
-    var search;
-    var translations;
+    var search,
+        translations;
 
     beforeEach(function() {
       translations = new En();
       search = new Search({
         translations: translations,
-        container: null,
-        api: {}
+        container: $('<div />'),
+        api: sinon.spy()
       });
     });
 
@@ -26,6 +30,24 @@ define(['locservices/ui/component/search', 'locservices/ui/translations/en'], fu
       it('should set this.eventNamespace to "locservices:ui:component:search"', function() {
         expect(search.eventNamespace).toBe('locservices:ui:component:search');
       });
+
+      it('should throw an exception if options do not api an element', function() {
+        var failure = function() {
+          new Search({
+            translations: translations,
+            container: null
+          });
+        };
+        expect(failure).toThrow();
+      });
+
+      it('should search for results when container is submitted');
+
+      it('should emit an event when a search starts');
+
+      it('should emit an event when a search end');
+
+      it('should emit an event when a search returns an error');
 
     });
 
